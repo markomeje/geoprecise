9aeb5e3f44278dd0613e086d379d47114e1e19ff
Your Name <onwabrains33@gmail.com>
2022-07-14 16:43:10 +0100

    layout Array

--- app/plots.php
+++ app/plots.php
@@ -2,8 +2,87 @@
 
 return [
 	'layouts' => [
-		'name' => '',
+		'name' => 'choice city layout',
 		'plots' => [
+			'residential'=>[
+				'name'=>'Residential plot'
+				'number'=>'1-1877',
+				]
+			'commercial'=>[
+				'name'=>'Commercial or Residential plot',
+				'number'=>'CR/1-CR/55',
+				]
+			'fullcommercial'=>[
+				'name'=>'Commercial plot',
+				'number'=>'C1-C6',
+				]
+			'public'=>[
+				'name'=>'Public plot',
+				'number'=>'P1-P17',
+			'openspace'=>[
+				'name'=>'Open Space',
+				'number'=>'OS/1-OS/4',
+				]
+		]
+
+		'name' => 'Peace layout',
+		'plots' => [
+			'residential'=>[
+				'name'=>'Residential plot'
+				'number'=>'1-401',
+				]
+			'commercial'=>[
+				'name'=>'Commercial or Residential plot',
+				'number'=>'CR/1-CR/11',
+				]
+			'openSpace'=>[
+				'name'=>'Open Space',
+				'number'=>'OS/1',
+				]
+			'public'=>[
+				'name'=>'Public plot',
+				'number'=>'P/1-P/2',
+				]
+			'utility'=>[
+				'name'=>'Utility Plot',
+				'number'=>'UT/1-UT/2',
+				]
+			'fullcommercial'=>[
+				'name'=>'Commercial Plot',
+				'number'=>'C/1-C/2',
+				]
+		]
+
+		'name' => 'Christa City Layout',
+		'plots' => [
+			'residential'=>[
+				'name'=>'Residential plot'
+				'number'=>'1-948',
+				]
+			'commercial'=>[
+				'name'=>'Commercial plot',
+				'number'=>'C/1-C/2',
+				]
+			'primary'=>[
+				'name'=>'Primary School',
+				'number'=>'P/1-P/2',
+				]
+			'worship'=>[
+				'name'=>'Place Of Worship',
+				'number'=>'P/3-P/4',
+				]
+			'security'=>[
+				'name'=>'Security',
+				'number'=>'P/5',
+				]
+			'health'=>[
+				'name'=>'Health Center',
+				'number'=>'P/6',
+				]
+			'refuse'=>[
+				'name'=>'Refuse Dump',
+				'number'=>'RD/1-RD/2',
+				]
 		]
 	]
 ]
