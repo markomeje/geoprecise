e4e767ccfffd7896c63f9e19d17f7f763d4487ce
markomeje <markomejeonline@gmail.com>
2022-08-22 16:03:09 +0100

    Fix Remote Work issues

--- database/factories/PsrFactory.php
+++ database/factories/PsrFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Factories;
-use App\Models\{Layout, Plot, User, Form};
+use App\Models\{Layout, Plot, User, Form, Psr};
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
--- database/seeders/FormSeeder.php
+++ database/seeders/FormSeeder.php
@@ -14,17 +14,17 @@ class FormSeeder extends Seeder
     public function run()
     {
         $forms = [
-            ['name' => 'Lifting or Extraction of Survey form', 'code' => 'LES', 'per' => 'Per Plot', 'amount' => 160000, 'description' => null, 'status' => 'active', 'category' => 'survey', 'payable' => true, 'reference' => Str::random(64)],
+            ['name' => 'Lifting or Extraction of Survey form', 'code' => 'LES', 'per' => 'Per Plot', 'amount' => 160000, 'status' => 'active', 'category' => 'survey', 'payable' => true],
 
-            ['name' => 'Change of title of Survey form', 'code' => 'CTS', 'per' => 'Per Plot', 'amount' => 160000, 'description' => null, 'status' => 'active', 'category' => 'survey', 'payable' => true, 'reference' => Str::random(64)],
+            ['name' => 'Change of title of Survey form', 'code' => 'CTS', 'per' => 'Per Plot', 'amount' => 160000, 'status' => 'active', 'category' => 'survey', 'payable' => true],
 
-            ['name' => 'Correction of name on Survey', 'code' => 'CNS', 'per' => 'Per Plot', 'amount' => 160000, 'description' => null, 'status' => 'active', 'category' => 'survey', 'payable' => true, 'reference' => Str::random(64)],
+            ['name' => 'Correction of name on Survey', 'code' => 'CNS', 'per' => 'Per Plot', 'amount' => 160000, 'status' => 'active', 'category' => 'survey', 'payable' => true],
 
-            ['name' => 'Site inspection or Beacon identification', 'code' => 'SIB', 'per' => 'Per Plot', 'amount' => 20000, 'description' => null, 'status' => 'active', 'category' => 'survey', 'payable' => true],
+            ['name' => 'Site inspection or Beacon identification', 'code' => 'SIB', 'per' => 'Per Plot', 'amount' => 20000, 'status' => 'active', 'category' => 'survey', 'payable' => true],
 
-            ['name' => 'Property Search Request form', 'code' => 'PSR', 'per' => 'Per Lodgement', 'amount' => 5000, 'description' => null, 'status' => 'active', 'category' => 'search', 'payable' => true, 'reference' => Str::random(64)],
+            ['name' => 'Property Search Request form', 'code' => 'PSR', 'per' => 'Per Lodgement', 'amount' => 5000, 'status' => 'active', 'category' => 'search', 'payable' => true],
 
-            ['name' => 'Plan Collection form', 'code' => 'PCO', 'per' => null, 'amount' => null, 'description' => null, 'status' => 'active', 'category' => 'plan', 'payable' => true, 'reference' => Str::random(64)],
+            ['name' => 'Plan Collection form', 'code' => 'PCO', 'per' => null, 'amount' => null, 'status' => 'active', 'category' => 'plan', 'payable' => true],
         ];
 
         Form::truncate();
