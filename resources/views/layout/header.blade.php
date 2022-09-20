aaf099877f2921e787bdf64ab066cad677fa15bd
Your Name <onwabrains33@gmail.com>
2022-09-20 16:17:30 +0100

    frontend

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -10,26 +10,26 @@
 <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
 <link rel="alternate" type="application/rss+xml" title="GeoPrecise &raquo; Feed" href="././feed/index.html">
 <link rel="alternate" type="application/rss+xml" title="GeoPrecise &raquo; Comments Feed" href="././comments/feed/index.html">
-<link rel="stylesheet" id="bootstrap-css" href="././wp-content/themes/kyber/assets/css/bootstrap.min.css?ver=4.0.0" type="text/css" media="all">
-<link rel="stylesheet" id="owl-carousel-css" href="././wp-content/themes/kyber/assets/css/owl.carousel.min.css?ver=2.3.4" type="text/css" media="all">
-<link rel="stylesheet" id="fontawesome-shims-css" href="././wp-content/themes/kyber/assets/fonts/css/v4-shims.min.css?ver=5.11.2" type="text/css" media="all">
-<link rel="stylesheet" id="fontawesome-css" href="././wp-content/themes/kyber/assets/fonts/css/all.min.css?ver=5.11.2" type="text/css" media="all">
-<link rel="stylesheet" id="flaticon-css" href="././wp-content/themes/kyber/assets/fonts/flaticon/flaticon.css?ver=1.0.6" type="text/css" media="all">
-<link rel="stylesheet" id="magnific-popup-css" href="././wp-content/themes/kyber/assets/css/magnific-popup.css?ver=1.1.0" type="text/css" media="all">
-<link rel="stylesheet" id="slicknav-css" href="././wp-content/themes/kyber/assets/css/slicknav.min.css?ver=1.0.10" type="text/css" media="all">
-<link rel="stylesheet" id="kyber-style-css" href="././wp-content/themes/kyber/style.css?ver=6.0" type="text/css" media="all">
-<link rel="stylesheet" id="kyber-theme-style-css" href="././wp-content/themes/kyber/assets/css/theme.css?ver=1.0.6" type="text/css" media="all">
-<link rel="stylesheet" id="js_composer_front-css" href="././wp-content/plugins/js_composer/assets/css/js_composer.min.css?ver=6.8.0" type="text/css" media="all">
+<link rel="stylesheet" id="bootstrap-css" href="././content/themes/kyber/assets/css/bootstrap.min.css?ver=4.0.0" type="text/css" media="all">
+<link rel="stylesheet" id="owl-carousel-css" href="././content/themes/kyber/assets/css/owl.carousel.min.css?ver=2.3.4" type="text/css" media="all">
+<link rel="stylesheet" id="fontawesome-shims-css" href="././content/themes/kyber/assets/fonts/css/v4-shims.min.css?ver=5.11.2" type="text/css" media="all">
+<link rel="stylesheet" id="fontawesome-css" href="././content/themes/kyber/assets/fonts/css/all.min.css?ver=5.11.2" type="text/css" media="all">
+<link rel="stylesheet" id="flaticon-css" href="././content/themes/kyber/assets/fonts/flaticon/flaticon.css?ver=1.0.6" type="text/css" media="all">
+<link rel="stylesheet" id="magnific-popup-css" href="././content/themes/kyber/assets/css/magnific-popup.css?ver=1.1.0" type="text/css" media="all">
+<link rel="stylesheet" id="slicknav-css" href="././content/themes/kyber/assets/css/slicknav.min.css?ver=1.0.10" type="text/css" media="all">
+<link rel="stylesheet" id="kyber-style-css" href="././content/themes/kyber/style.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="kyber-theme-style-css" href="././content/themes/kyber/assets/css/theme.css?ver=1.0.6" type="text/css" media="all">
+<link rel="stylesheet" id="js_composer_front-css" href="././content/plugins/js_composer/assets/css/js_composer.min.css?ver=6.8.0" type="text/css" media="all">
 <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Rajdhani:700&#038;display=swap&#038;ver=1647434742">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rajdhani:700&#038;display=swap&#038;ver=1647434742" media="print" onload="this.media='all'">
 <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rajdhani:700&#038;display=swap&#038;ver=1647434742"></noscript>
-<script type="text/javascript" src="././wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script type="text/javascript" src="././wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
-<script type="text/javascript" src="././wp-content/themes/kyber/assets/js/slicknav.min.js?ver=1.0.10" id="slicknav-js"></script>
+<script type="text/javascript" src="././includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="././includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script type="text/javascript" src="././content/themes/kyber/assets/js/slicknav.min.js?ver=1.0.10" id="slicknav-js"></script>
 <link rel="https://api.w.org/" href="././wp-json/index.html">
 <link rel="alternate" type="application/json" href="././wp-json/wp/v2/pages/144/index.html">
 <link rel="EditURI" type="application/rsd+xml" title="RSD" href="././xmlrpc.php?rsd">
-<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="././wp-includes/wlwmanifest.xml"> 
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="././includes/wlwmanifest.xml"> 
 <meta name="generator" content="WordPress 6.0">
 <link rel="canonical" href="././index.html">
 <link rel="shortlink" href="././index.html">
@@ -38,7 +38,7 @@
 <meta name="generator" content="Redux 4.3.14">
 <meta name="generator" content="Powered by WPBakery Page Builder - drag and drop page builder for WordPress.">
 <meta name="generator" content="Powered by Slider Revolution 6.5.14 - responsive, Mobile-Friendly Slider Plugin for WordPress with comfortable drag and drop interface.">
-<link rel="icon" href="././wp-content/uploads/2022/02/cropped-favicon-32x32.png" sizes="32x32">
-<link rel="icon" href="././wp-content/uploads/2022/02/cropped-favicon-192x192.png" sizes="192x192">
-<link rel="apple-touch-icon" href="././wp-content/uploads/2022/02/cropped-favicon-180x180.png">
-<meta name="msapplication-TileImage" content="././wp-content/uploads/2022/02/cropped-favicon-270x270.png">
+<link rel="icon" href="././content/uploads/2022/02/cropped-favicon-32x32.png" sizes="32x32">
+<link rel="icon" href="././content/uploads/2022/02/cropped-favicon-192x192.png" sizes="192x192">
+<link rel="apple-touch-icon" href="././content/uploads/2022/02/cropped-favicon-180x180.png">
+<meta name="msapplication-TileImage" content="././content/uploads/2022/02/cropped-favicon-270x270.png">
