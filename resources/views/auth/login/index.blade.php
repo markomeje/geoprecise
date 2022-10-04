b3aa256b1d74ced31fc95175292aa4fcdd843ba6
markomeje <markomejeonline@gmail.com>
2022-10-04 12:05:24 +0100

    Update Login  page design

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -48,7 +48,7 @@
               </div>
             </div>
             <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 justify-content-center flex-column">
-              <div class="position-relative bg-gradient-dark h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('/images/scraper.jpg'); background-size: cover;">
+              <div class="position-relative bg-gradient-dark h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('./../../content/uploads/2020/04/Remote-Sensing.jpg'); background-size: cover;">
                 <span class="mask bg-gradient-dark opacity-6"></span>
                 <h4 class="mt-5 text-white font-weight-bolder position-relative">{{ config('app.name') }}</h4>
                 <div class="text-white position-relative">Since 1973, Geoprecise Services Limited has been the premier Boundary Surveyor in South-East Nigeria. Therefore, we will deliver a fast turnaround, in comparison to our competitors, that will help you meet your critical project deadlines.</div>
