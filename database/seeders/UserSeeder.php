4e1b5dc357454037498e11dc41fceae82f684b1d
markomeje <markomejeonline@gmail.com>
2023-01-12 10:46:17 +0100

    Remove user Seeded details

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,45 +15,9 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
-        $users = [
-            ['role' => 'admin', 'phone' => '08158212666', 'email' => 'admin@admin.io', 'password' => Hash::make('1234!.')],
-            ['role' => 'client', 'phone' => '08087752375', 'email' => 'user@user.io', 'password' => Hash::make('1234!.')],
-        ];
+        if (app()->environment(['local'])) {}
 
-        User::truncate();
-        foreach ($users as $user) {
-            $user = User::create($user);
-            if ($user->role === 'client') {
-                Client::create([
-                    "user_id" => $user->id,
-                    'fullname' => $faker->name(),
-                    'dob' => $faker->date(),
-                    'title' => $faker->title(),
-                    'occupation' => $faker->jobTitle(),
-                    'address' => $faker->address(),
-                    'status' => 'incomplete',
-                ]);
-            }
-        }
-
-        if (app()->environment(['production'])) {
-            // User::create([
-            //     'phone' => '08063388846', 
-            //     'email' => 'nnam.ug@gmail.com', 
-            //     'role' => 'admin', 
-            //     'password' => Hash::make('!ow-pre@360'), 
-            //     'status' => 'active'
-            // ]);
-
-            User::create([
-                'phone' => '08158212666', 
-                'email' => 'markomejeonline@gmail.com', 
-                'role' => 'admin', 
-                'password' => Hash::make('!tochukwu360@!.'), 
-                'status' => 'active'
-            ]);
-        }
+        if (app()->environment(['production'])) {}
     }
 }
 
